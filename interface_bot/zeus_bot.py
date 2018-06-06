5d7960b8b14bf17e734699e9f46744a488bb5993
CJHackerz <cjhackerz@outlook.com>
2018-06-07 05:12:02 +0530

    basic code base for zeus bot to keep eye on live chat feed

---
+++ interface_bot/zeus_bot.py
@@ -0,0 +1,17 @@
+import json
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+zeus_bot = Updater('511574247:AAH3erAoX0XlV7R0cuVMxx_pbI_vbE2DtU8')
+
+def watchMessages(bot, update):
+    group_name = update.message.chat.username
+    username = update.message.from_user.username 
+    text_content = update.message.text
+    json_dir = {"group_name": group_name, "username": username, "chat": text_content}
+    json_obj = json.dumps(json_dir)
+    print(json_obj)
+
+zeus_bot.dispatcher.add_handler(MessageHandler(Filters.text, watchMessages))
+
+zeus_bot.start_polling()
+zeus_bot.idle()

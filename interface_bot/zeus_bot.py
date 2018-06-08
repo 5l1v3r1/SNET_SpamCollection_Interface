e1bd46c0ac69e96006bfd948398c06b49cb719c7
CJHackerz <cjhackerz@outlook.com>
2018-06-08 05:34:43 +0530

    Added API endpoint communication feature in zeus bot

--- interface_bot/zeus_bot.py
+++ interface_bot/zeus_bot.py
@@ -1,8 +1,10 @@
 import json
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import httplib2
 
 BOT_API_TOKEN = ''
 zeus_bot = Updater(BOT_API_TOKEN)
+API_ENDPOINT_URL = ''
 
 def watchMessages(bot, update):
     group_name = update.message.chat.username
@@ -10,7 +12,14 @@ def watchMessages(bot, update):
     text_content = update.message.text
     json_dir = {"group_name": group_name, "username": username, "chat": text_content}
     json_obj = json.dumps(json_dir)
-    print(json_obj)
+    http = httplib2.Http()
+    resp, content = http.request(
+        uri=API_ENDPOINT_URL,
+        method='POST',
+        headers={'Content-Type': 'application/json'},
+        body=json_obj
+    )
+
 
 zeus_bot.dispatcher.add_handler(MessageHandler(Filters.text, watchMessages))
 

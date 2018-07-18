e4a0f28b1bed2a7fcd444abed8f5858d7743e0ec
CJHackerz <cjhackerz@outlook.com>
2018-07-18 15:03:53 +0530

    Ingnoring to store bot responses to database...

--- interface_bot/zeus_bot.py
+++ interface_bot/zeus_bot.py
@@ -2,7 +2,7 @@ import json
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import httplib2
 
-BOT_API_TOKEN = '511574247:AAGE5ggP69zGHwCHF1Y6cOp4LS3LrBkZYLY'
+BOT_API_TOKEN = ''
 zeus_bot = Updater(BOT_API_TOKEN)
 API_ENDPOINT_URL = 'http://localhost:8000/api/'
 
@@ -12,15 +12,16 @@ def watchMessages(bot, update):
     text_content = update.message.text
     json_dir = {"group_name": group_name, "username": username, "text_content": text_content}
     json_obj = json.dumps(json_dir)
-    print(json_obj)
-    http = httplib2.Http()
-    resp, content = http.request(
-        uri=API_ENDPOINT_URL,
-        method='POST',
-        headers={'Content-Type': 'application/json'},
-        body=json_obj
-    )
-    print(resp)
+    if username != "Zeus_A095F_BOT":
+        print(json_obj)
+        http = httplib2.Http()
+        resp, content = http.request(
+            uri=API_ENDPOINT_URL,
+            method='POST',
+            headers={'Content-Type': 'application/json'},
+            body=json_obj
+        )
+        print(resp)
     #print(content)
 
 zeus_bot.dispatcher.add_handler(MessageHandler(Filters.text, watchMessages))
